f198581461f6f5d62bee9cc51e95f9a3bd5fad4b
Prakya Kuncham <prakyakuncham@gmail.com>
2026-04-10 14:25:12 +0530

    Fix debug flag case in app.run

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app=Flask(__name__)
 def home():
   return render_template('Index.html', title='Home Page')
 if __name__=='__main__':
-  app.run(host='271.0.0.1', port=8080, debug=true)
+  app.run(host='271.0.0.1', port=8080, debug=True)
